b2e74b8bebcb7153704ae6f1f9792ddde20772b5
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-07 00:15:44 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,3 @@
 def reverse_each_word(phrase)
-  reverse_phrase = ""
-  phrase.split.collect{|word| reverse_phrase << "#{word.reverse} "}
-  reverse_phrase.strip
+  phrase.split.collect{|word| word.reverse}
 end
